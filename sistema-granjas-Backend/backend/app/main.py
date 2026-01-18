f56b54b4f873a20304d9ff2fdcd83cacaedfefd1
MarlonHerrera01 <marlon.1701914781@ucaldas.edu.co>
2026-01-18 16:56:42 -0500

    Agrega url de conexion back front

--- sistema-granjas-Backend/backend/app/main.py
+++ sistema-granjas-Backend/backend/app/main.py
@@ -76,7 +76,7 @@ app = FastAPI(
 # CORS - Añade tu dominio de frontend si está en producción
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173", "https://sistema-granjas-ucaldas-front.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
