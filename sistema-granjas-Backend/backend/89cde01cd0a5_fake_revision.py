9983bbcbf329c616aa79d8da6c1df28dad1798a6
Checho0507 <checho.mlj@gmail.com>
2026-05-06 19:32:38 -0500

    diagnostico_campos

---
+++ sistema-granjas-Backend/backend/89cde01cd0a5_fake_revision.py
@@ -0,0 +1,20 @@
+"""fake revision to align with existing database state
+
+Revision ID: 89cde01cd0a5
+Revises: 
+Create Date: 2024-01-01 00:00:00.000000
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers
+revision = '89cde01cd0a5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
