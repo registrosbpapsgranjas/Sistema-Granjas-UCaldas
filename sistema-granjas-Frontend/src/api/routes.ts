0e962ff5fc8d505855e2f82b5d4d9e4f00f50e4b
Checho0507 <checho.mlj@gmail.com>
2026-06-29 17:26:40 -0500

    endpoints

--- sistema-granjas-Frontend/src/api/routes.ts
+++ sistema-granjas-Frontend/src/api/routes.ts
@@ -1,7 +1,7 @@
 // Define la URL base de tu backend (FastAPI)
 export const BASE_URL = "https://127.0.0.1:8000";
 
-// Define los endpoints específicos
+// Define los endpoints específicos login
 export const ENDPOINTS = {
     createGranja: `${BASE_URL}/api/granjas`,
 };
